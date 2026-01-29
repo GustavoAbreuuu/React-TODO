808cf875e9e24c13e6dc150690c79d38af61e70e
GustavoAbreuuu <gustavo.abreu2009@gmail.com>
2026-01-29 08:07:45 -0300

    feat: implement core todo list logic and styling
    - Implement state management for adding and listing todos
    
    - Add input handling with Enter (submit) and Escape (clear) keys
    
    - Integrate react-icons for UI elements
    
    - Apply global styles and specific component styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,66 @@
+import { useState } from 'react';
+import { MdDelete} from 'react-icons/md';
 import './App.css';
 
-const App = () => (
-  <h1 className='title'>App</h1>
-);
+const App = () => {
+  const ESCAPE_KEY = 27;
+  const ENTER_KEY = 13;
 
-export default App
+  const [todos, setTodos ] = useState([]);
+  const [value, setValue] = useState(''); 
+
+  const erase = () => {
+    setValue(''); 
+  };
+
+  const submit = () => {
+    setTodos([
+      ...todos, 
+      {
+        id: new Date().getTime(), 
+        title: value, 
+        checked: false,
+      }
+    ]);
+
+    erase();
+  };
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  };
+
+  const onKeyDown = (event) => {
+    if (event.which === ENTER_KEY) {
+      submit();
+    } else if (event.which === ESCAPE_KEY) {
+      erase();
+    }
+  };
+
+  return(
+    <div id='app' className='container'>
+      <header>
+        <h1 className='title'>todo</h1>
+      </header>
+
+      <main>
+        <input type="text" className='new-todo' placeholder='o que precisa ser feito?' value={value}
+        onChange={onChange} onKeyDown={onKeyDown} />
+        <ul className='todo-list'>
+          {todos.map((todo) => (
+            <li key={todo.id.toString()}>
+              <span className='todo'>{todo.title}</span>
+              <button className='remove' type='button'> 
+                <MdDelete size={28} />
+              </button>
+            </li>
+            ))}
+        </ul>
+      </main>
+    </div>
+  ); 
+}
+
+export default App;
 
